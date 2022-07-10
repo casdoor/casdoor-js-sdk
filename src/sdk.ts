92d34ad8c2bcce29a7102bfba7b78e1243f91d12
Yixiang Zhao <674965440@qq.com>
2022-07-10 11:52:29 +0800

    feat: store signin url when getting the signup url (#30)
    * feat: store login url when getting the signup url
    
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>
    
    * Update sdk.ts
    
    Co-authored-by: Yang Luo <hsluoyz@qq.com>

--- src/sdk.ts
+++ src/sdk.ts
@@ -49,6 +49,7 @@ class Sdk {
 
     public getSignupUrl(enablePassword: boolean = true): string {
         if (enablePassword) {
+            sessionStorage.setItem("signinUrl", this.getSigninUrl());
             return `${this.config.serverUrl.trim()}/signup/${this.config.appName}`;
         } else {
             return this.getSigninUrl().replace("/login/oauth/authorize", "/signup/oauth/authorize");
