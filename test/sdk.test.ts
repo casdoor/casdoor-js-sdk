a5a20b702b353827e413be2529fca2423e6902c5
jump2cn <jump2cn@gmail.com>
2023-04-23 17:33:35 +0800

    to: add test case

--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -1,11 +1,11 @@
 import Sdk from '../src';
-  const sdkConfig = {
-    serverUrl: 'https://door.casbin.com',
-    clientId: '014ae4bd048734ca2dea',
-    appName: 'app-casnode',
-    organizationName: 'casbin',
-    redirectPath: '/callback',
-  };
+const sdkConfig = {
+  serverUrl: 'https://door.casbin.com',
+  clientId: '014ae4bd048734ca2dea',
+  appName: 'app-casnode',
+  organizationName: 'casbin',
+  redirectPath: '/callback',
+};
 describe('sdk constructor', () => {
   it('with full configs', () => {
     const sdk = new Sdk(sdkConfig);
@@ -31,6 +31,10 @@ describe('sdk constructor', () => {
 });
 
 describe('getSigninUrl', () => {
+  beforeEach(() => {
+    sessionStorage.clear();
+  });
+
   it('redirectPath with relative path', () => {
     const sdk = new Sdk(sdkConfig);
 
@@ -51,5 +55,19 @@ describe('getSigninUrl', () => {
     expect(sdk.getSigninUrl()).toContain(
       `redirect_uri=${encodeURIComponent(config.redirectPath)}`
     );
+  });
+
+  it('with fixed state', () => {
+    const state = 'test-state';
+    const sdk = new Sdk(sdkConfig);
+    sessionStorage.setItem('casdoor-state', state);
+    expect(sdk.getSigninUrl()).toContain(`state=${state}`);
+  });
+
+  it('with random state', () => {
+    const sdk = new Sdk(sdkConfig);
+    const url = sdk.getSigninUrl();
+    const state = sessionStorage.getItem('casdoor-state');
+    expect(url).toContain(`state=${state}`);
   });
 });
